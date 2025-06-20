8d5596386e17918acd186c5ee7673872b2ec45ff
Yousuf <abuyousufsiam7249@gmail.com>
2025-06-21 02:23:46 +0600

    react native app created and launched

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center"
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+        <Text className="text-5xl text-primary font-bold">This is the first app build using React Native.</Text>
+        <Text>Developer: Md. Abu Yousuf Siam</Text>
+    </View>
+  );
+}
